eec9c4333e11177c753f1bb66193e2283ef24a02
Mingi Hong <escandar@naver.com>
2022-08-10 23:35:26 +0900

    tick 정보 설정파일 생성

---
+++ strategy/item_tick_info.py
@@ -0,0 +1,50 @@
+ITEM_TICK_INFO = {
+    "cocoa": {
+        'tick_size': 1,
+        'tick_value': 10
+    },
+    "bitcoin": {
+        'tick_size': 2.5,
+        'tick_value': 2.5
+    },
+    "coffee": {
+        'tick_size': 0.05,
+        'tick_value': 18.75
+    },
+    "corn": {
+        'tick_size': 0.25,
+        'tick_value': 12.5
+    },
+    "dow": {
+        'tick_size': 1,
+        'tick_value': 0.5
+    },
+    "euro_dollar": {
+        'tick_size': 0,
+        'tick_value': 0
+    },
+    "gold": {
+        'tick_size': 0.1,
+        'tick_value': 1
+    },
+    "nasdaq": {
+        'tick_size': 0.25,
+        'tick_value': 0.5
+    },
+    "natural_gas": {
+        'tick_size': 0.05,
+        'tick_value': 12.5
+    },
+    "pork": {
+        'tick_size': 0.025,
+        'tick_value': 10
+    },
+    "wheat": {
+        'tick_size': 0.25,
+        'tick_value': 12.5
+    },
+    "wti_oil": {
+        'tick_size': 0.01,
+        'tick_value': 1
+    },
+}
