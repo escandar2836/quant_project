b1f2744ea187eb54d68319b4413fc6760aee6986
Mingi Hong <escandar@naver.com>
2023-01-12 21:15:13 +0900

    기본 s1 turtle trading 구현

--- backtest/turtle_stg.py
+++ backtest/turtle_stg.py
@@ -36,35 +36,34 @@ class TurtleStrategy(SignalStrategy):
             return
 
         for order in self.orders:
+            if order.is_contingent:
+                continue
             order.cancel()
 
-        # if self.closed_trades and len(self.closed_trades) != self.closed_trades_count:
-        #     self.closed_trades_count = len(self.closed_trades)
-        #     print(1)
         breakout_price_dict = self.check_breakout_price()
 
         # position이 있다면
         if self.position:
             # 걸려있던 주문 취소하고 2주 돌파값으로 반대방향 주문
             if self.position.is_long:
-                self.sell(size=self.position.size, stop=breakout_price_dict['buy_sl'])
+                self.trades[0].sl = max(self.trades[0].sl, breakout_price_dict['buy_sl'])
             else:
-                self.buy(size=self.position.size, stop=breakout_price_dict['sell_sl'])
+                self.trades[0].sl = min(self.trades[0].sl, breakout_price_dict['sell_sl'])
         # position이 없다면
         else:
             position_size = self.calculate_position_size()
             if not position_size:
                 return
             # 걸려있던 주문 취소하고 4주 돌파값으로 주문
-            self.buy(size=position_size, stop=breakout_price_dict['buy_breakout'], sl=breakout_price_dict['buy_breakout'] - 2*self.atr[-1])
-            self.sell(size=position_size, stop=breakout_price_dict['sell_breakout'], sl=breakout_price_dict['sell_breakout'] + 2*self.atr[-1])
+            self.buy(size=position_size, stop=breakout_price_dict['buy_breakout'], sl=breakout_price_dict['buy_breakout']-2*self.atr[-1])
+            self.sell(size=position_size, stop=breakout_price_dict['sell_breakout'], sl=breakout_price_dict['sell_breakout']+2*self.atr[-1])
 
     def check_breakout_price(self):
         return {
-            'buy_breakout': max(self.data.High[max(0, -1 - self.n1):-1]),
-            'sell_breakout': min(self.data.Low[max(0, -1 - self.n1):-1]),
-            'buy_sl': max(self.data.Low[max(0, -1 - self.n1 // 2):-1]),
-            'sell_sl': min(self.data.High[max(0, -1 - self.n1 // 2):-1])
+            'buy_breakout': max(self.data.High[-1 - self.n1:-1]),
+            'sell_breakout': min(self.data.Low[-1 - self.n1:-1]),
+            'buy_sl': min(self.data.Low[-1 - self.n1 // 2:-1]),
+            'sell_sl': max(self.data.High[-1 - self.n1 // 2:-1])
         }
 
     def calculate_position_size(self):
